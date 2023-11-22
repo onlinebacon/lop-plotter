08e2bd7718e6ba919f3319161ef0dfe04a0b309b
Bacon <bacon.antife@gmail.com>
2023-11-22 04:15:02 -0300

    wip

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ import { D180, D360, D90, DEG } from "./lib/trig.js";
 
 const canvas = document.querySelector('canvas');
 const lopArr = [];
+let minErrDif = 0;
+let minErrColor = '#fff';
 
 const parseDegToRadian = (string) => {
 	return parseDegree(string)/180*Math.PI;
@@ -24,7 +26,9 @@ const img = await loadImage('img/map.jpg');
 const colorPicker = new ColorPicker(img);
 
 const defInput = `
-	lat=-25.5, lon=-54.5, rad=1, azm=45, color=#07f
+	lat=45 23 15.5 N, lon=12 30 42.0 W, rad=71 42 11,  dif=0.5, color=#07f
+	lat=30 57 16.7 S, lon=63 15 22.3 E, azm=183 23 10, dif=0.5, color=#f70
+	min-err dif=1, color=#fff
 `.trim().split('\n').map(line => line.trim()).join('\n');
 
 const textarea = document.querySelector('textarea');
@@ -33,9 +37,24 @@ textarea.value = defInput;
 const loadInput = () => {
 	lopArr.length = 0;
 	const lines = textarea.value.split('\n');
+	minErrDif = 0;
+	minErrColor = '#fff';
 	for (let line of lines) {
 		line = line.trim();
 		if (line === '') continue;
+		if (line.startsWith('min-err')) {
+			line = line.replace('min-err', '').trim();
+			line.split(/\s*,\s*/).forEach(pair => {
+				const [ key, val ] = pair.split(/\s*=\s*/);
+				if (key === 'dif') {
+					minErrDif = parseDegToRadian(val);
+				}
+				if (key === 'color') {
+					minErrColor = val;
+				}
+			});
+			continue;
+		}
 		const input = {
 			latLon: [ NaN, NaN ],
 			rad: null,
@@ -53,7 +72,6 @@ const loadInput = () => {
 		});
 		lopArr.push(input);
 	}
-	console.log(lopArr);
 };
 
 loadInput();
@@ -65,21 +83,37 @@ const frag = new Frag({
 			return '#000';
 		}
 		const coord = [ y*D180 - D90, x*D360 - D180 ];
+		let sumErr = 0;
+		let errCount = 0;
+		let color = null;
 		for (const lop of lopArr) {
 			if (lop.rad !== null) {
 				const rad = haversine(coord, lop.latLon);
 				const err = Math.abs(lop.rad - rad);
+				sumErr += err**2;
+				errCount ++;
 				if (err <= lop.dif) {
-					return lop.color;
+					color = lop.color;
 				}
 			}
 			if (lop.azm !== null) {
 				const azm = calcAzimuth(coord, lop.latLon);
 				const err = Math.abs(lop.azm - azm);
+				sumErr += err**2;
+				errCount ++;
 				if (err <= lop.dif) {
-					return lop.color;
+					color = lop.color;
 				}
 			}
+		}
+		if (minErrDif !== 0 && errCount !== 0) {
+			const error = Math.sqrt(sumErr/errCount);
+			if (error < minErrDif) {
+				return minErrColor;
+			}
+		}
+		if (color !== null) {
+			return color;
 		}
 		const col = x*img.width|0;
 		const row = (1 - y)*img.height|0;
@@ -94,3 +128,12 @@ textarea.oninput = () => {
 	loadInput();
 	frag.render();
 };
+
+canvas.addEventListener('dblclick', e => {
+	const x = e.offsetX;
+	const y = e.offsetY;
+	const [ nx, ny ] = frag.valueOf(x, y);
+	const lat = ny*D180 - D90;
+	const lon = nx*D360 - D180;
+	document.querySelector('input').value = [ lat, lon ].map(val => val/Math.PI*180);
+});
